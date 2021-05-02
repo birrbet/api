5ccc68496381ac28fc705ab3caa2e5a8ce8f2681
ezedinff <ezedin.fedlu@gmail.com>
2021-05-02 14:51:23 +0300

    update winston config

--- src/infrastructure/configs/winston.config.ts
+++ src/infrastructure/configs/winston.config.ts
@@ -6,22 +6,32 @@ import { RabbitMqService } from "../rabbit-mq/rabbit-md.module";
 
 const consoleTransportFactory = (level: string) => new transports.Console({
     level,
-    format: format.combine(format.colorize(), format.simple())
+    format: format.combine(
+                format.colorize(),
+                format.simple(),
+                format.timestamp(),
+                format.json(),
+                format.errors({ stack: true })
+            )
 });
+export const LEVEL = 'info';
+/**
+ * Defines configuration for logging
+ */
 @Injectable()
 export class WinstonConfig implements WinstonModuleOptionsFactory {
     constructor(private rabbitMqService: RabbitMqService) {}
     
     createWinstonModuleOptions(): Promise<WinstonModuleOptions> | WinstonModuleOptions {
         return {
-            level: 'info',
+            level: LEVEL,
             exitOnError: false,
             transports: [
                 // @TODO implement custom transports to integrate with message queue
                 new RabbitWinstonTransport(this.rabbitMqService, undefined, {
                     level: 'info'
                 }),
-                //...['info', 'error'].map(level => consoleTransportFactory(level))
+                ...['info', 'error'].map(level => consoleTransportFactory(level))
             ]
         }
     }
