5fec7429a6927be4c6e90fde3898c7215fe923ef
Ezedin <ezedin.fedlu@gmail.com>
2021-05-11 11:09:27 +0300

    add csurf

--- src/main.ts
+++ src/main.ts
@@ -7,6 +7,7 @@ import * as helmet from 'helmet';
 import * as cookieParser from 'cookie-parser';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import { ConfigService } from '@nestjs/config';
+import * as csurf from 'csurf';
 // import * as rateLimit from 'express-rate-limit';
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
@@ -15,6 +16,8 @@ async function bootstrap() {
   app.use(compression());
   // app.use(helmet());
   app.use(cookieParser());
+  // because we're using cookie we need to defend from cross site forgery attack
+  app.use(csurf());
   const logger = app.get(WINSTON_MODULE_NEST_PROVIDER);
   app.useLogger(logger);
   const configService = app.get<ConfigService>(ConfigService);
